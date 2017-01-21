13a0ba61a75b35edcdeef72dc7733e36ce84b46b
Kevin Tonon <kevin@betweenconcepts.com>
2017-01-20 20:55:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+AWS SDK for elm
+===============
+
+__Experimental: Work in progress__
+
+This will be generated.
+
+The plan is to generate the elm code from the AWS SDK for JavaScript package.
+It contains `apis/*.json` describing all the types and functions used in the SDK.
---
+++ src/AWS.elm
@@ -0,0 +1,18 @@
+module AWS exposing (..)
+
+{-| AWS SDK for elm.
+
+__Experimental: Work in progress__
+
+@docs NotYetImplemented
+
+-}
+
+
+{-| This will be generated.
+
+The plan is to generate the elm code from the AWS SDK for JavaScript package.
+It contains `apis/*.json` describing all the types and functions used in the SDK.
+-}
+type alias NotYetImplemented =
+    ()
