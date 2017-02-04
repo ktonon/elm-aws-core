16a7141e67b91a1a681ef0e34fc4eb110b727cb5
Kevin Tonon <kevin@betweenconcepts.com>
2017-02-04 15:00:29 -0500

    Generate functions from operations list. So far only the name and link to js docs are rendered correctly, the signature is just set to String

--- scripts/process-service.js
+++ scripts/process-service.js
@@ -1,15 +1,37 @@
+const camelize = require('camelize');
 const fs = require('fs');
 const sysPath = require('path');
 const dots = require('./util/dots');
 const moduleName = require('./util/module-name');
 
+const lowCam = (x) => {
+  const y = camelize(x);
+  return y[0].toLowerCase() + y.slice(1);
+};
+
 const outRoot = sysPath.resolve(`${__dirname}/../src/AWS`);
+const docsRoot = 'http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS';
 
 module.exports = (data) => {
-  data.moduleName = moduleName(data.metadata);
+  const mod = moduleName(data.metadata);
+  const opNames = Object.keys(data.operations).map(lowCam);
+  const context = {
+    apiVersion: data.metadata.apiVersion,
+    link: `${docsRoot}/${mod}.html`,
+    mod,
+    opNames,
+    ops: Object.keys(data.operations).map((key) => {
+      const name = lowCam(key);
+      return {
+        name,
+        link: `${docsRoot}/${mod}.html#${name}-property`,
+      };
+    }),
+    serviceFullName: data.metadata.serviceFullName,
+  };
   fs.writeFileSync(
-    `${outRoot}/${data.moduleName}.elm`,
-    dots.api(data),
+    `${outRoot}/${mod}.elm`,
+    dots.api(context),
     'utf8');
-  return data;
+  return context;
 };
