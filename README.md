daa44a12005211ff051be5bc63725efe5a6c13d5
Kevin Tonon <kevin@betweenconcepts.com>
2017-02-03 21:00:15 -0500

    Read and son parse all .min.json api files

--- README.md
+++ README.md
@@ -5,10 +5,7 @@ AWS SDK for elm
 
 __Experimental: Work in progress__
 
-This will be generated.
-
-The plan is to generate the elm code from the [AWS SDK for JavaScript][] package.
-It contains [apis/*.json][] describing all the types and functions used in the SDK.
+This repo contains scripts which read and translate the AWS SDK [apis/*.json][] files into elm packages.
 
 [apis/*.json]:https://github.com/aws/aws-sdk-js/tree/master/apis
 [AWS SDK for JavaScript]:https://github.com/aws/aws-sdk-js
---
+++ scripts/generate-sdk.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const sysPath = require('path');
+const processService = require('./process-service');
+
+const serviceRoot = sysPath.resolve(`${__dirname}/../node_modules/aws-sdk/apis`);
+if (!fs.existsSync(serviceRoot)) {
+  console.log('Run `npm install` before running this script');
+  process.exit(1);
+}
+
+const jsonSources = fs.readdirSync(serviceRoot)
+  .filter(path => path.endsWith('.min.json'))
+  // .filter(path => /dynamodb-2012/.test(path))
+  ;
+
+jsonSources.forEach((filename) => {
+  const path = `${serviceRoot}/${filename}`;
+  if (fs.existsSync(path)) {
+    try {
+      const desc = JSON.parse(fs.readFileSync(path));
+      processService(desc);
+    } catch (err) {
+      console.error(`error processing ${filename}: ${err}`);
+    }
+  } else {
+    console.error(`no such file: ${filename}`);
+  }
+});
