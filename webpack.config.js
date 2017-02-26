d52378a64cdd22272dc892dcdd5068bb5f2df72f
Kevin Tonon <kevin@betweenconcepts.com>
2017-02-26 12:26:09 -0500

    Fix linter issues in webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
-/* eslint no-use-before-define: 0 */
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-    entry: './src/Native/HMAC.pack.js',
-    output: {
-        filename: 'HMAC.out.js',
-        path: path.resolve(__dirname, 'src/Native')
-    },
-    resolve: {
-        modules: [
-            "node_modules"
-        ]
-    }
+  entry: './src/Native/HMAC.pack.js',
+  output: {
+    filename: 'HMAC.out.js',
+    path: path.resolve(__dirname, 'src/Native'),
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+    ],
+  },
 };
